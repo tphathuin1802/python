2fbedb98940b0730caea8fb2e14dea013dd6965a
tphathuin1802 <150665582+tphathuin1802@users.noreply.github.com>
2023-11-18 16:46:02 +0700

    first commit

---
+++ python/python.py
@@ -0,0 +1,7 @@
+def tinhdiemtrungbinh():
+    toan = float(input('diemtoan la:'))
+    hoa = float(input('diemhoa la:'))
+    ly = float(input('diemly la:'))
+    anh = float(input('diemanh la:'))
+    trungbinh = (toan + hoa + ly + anh) / 4
+    return trungbinh
